6ec67033a84ec2fc11aad6927d2673b3ae64f00c
Aleksey Krupsky <a3.krupsky@itransition.com>
2023-12-01 18:10:26 +0300

    TMP. Add terms form

---
+++ app/src/Controller/TermController.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Term2;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TermController extends AbstractController
+{
+    private ManagerRegistry $registry;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    #[Route('/terms/add', name: 'addTerm', methods: 'GET')]
+    public function showAddForm(): Response
+    {
+        return $this->render('terms.add.html.twig');
+    }
+
+    #[Route('/terms/add', methods: 'POST')]
+    public function saveTerm(
+        #[MapQueryParameter] string $term,
+        #[MapQueryParameter] string $translation,
+    ): Response
+    {
+
+        $entityManager = $this->registry->getManagerForClass(Term2::class);
+
+        $newTerm = new Term2();
+        $newTerm->term = $term;
+        $newTerm->translation = $translation;
+
+        $entityManager->persist($newTerm);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('addTerm');
+    }
+}
---
+++ app/src/Entity/Term.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TermRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TermRepository::class)]
+class Term
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $term = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $translation = null;
+
+    #[ORM\Column]
+    private ?bool $learned = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTerm(): ?string
+    {
+        return $this->term;
+    }
+
+    public function setTerm(string $term): static
+    {
+        $this->term = $term;
+
+        return $this;
+    }
+
+    public function getTranslation(): ?string
+    {
+        return $this->translation;
+    }
+
+    public function setTranslation(string $translation): static
+    {
+        $this->translation = $translation;
+
+        return $this;
+    }
+
+    public function isLearned(): ?bool
+    {
+        return $this->learned;
+    }
+
+    public function setLearned(bool $learned): static
+    {
+        $this->learned = $learned;
+
+        return $this;
+    }
+}
